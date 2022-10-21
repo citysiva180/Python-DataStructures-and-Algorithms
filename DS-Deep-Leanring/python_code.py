468ba5bc9a448be9b5f60271e1125c58ee613d1c
Sivarajan <citysiva180@users.noreply.github.com>
2022-10-21 11:18:23 +0530

    Learning Code

---
+++ DS-Deep-Leanring/python_code.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+
+
+def print_items(n):
+    for i in range(n):
+        print(i)
+
+
+print_items(10)
+# Compared to 10 10 million will make this algorithm a Omicron algorithm
+print_items(1000000)
