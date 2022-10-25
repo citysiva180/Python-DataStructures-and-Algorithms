4f1f0fc1811db3873acdbcf2c307b0791f293d7b
Sivarajan <citysiva180@users.noreply.github.com>
2022-10-25 12:12:09 +0530

    Linked List completed final

--- DS-Deep-Leanring/linked_list.py
+++ DS-Deep-Leanring/linked_list.py
@@ -8,8 +8,6 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-
-
 class LinkedList:
 
     # Linked List Constructor
--- self-learning-python/collection_named_tuple.py
+++ self-learning-python/collection_named_tuple.py
@@ -13,7 +13,7 @@ print(prime_cars)
 
 celebrity_dict = namedtuple(
     'a_list', {'new_face': "", 'singer': "", 'queen_bey': ""})
-a_list_celebs = celebrity_dict('Bradly Cooper', 'Lady Gaga', 'beyonce')
+a_list_celebs = celebrity_dict('Bradly Cooper', 'Lady Gaga', 'Beyonce')
 print(a_list_celebs.new_face, a_list_celebs.singer, a_list_celebs.queen_bey)
 print(a_list_celebs._fields)
 new_celeb_list = a_list_celebs._replace(new_face='Tom Cruise')
