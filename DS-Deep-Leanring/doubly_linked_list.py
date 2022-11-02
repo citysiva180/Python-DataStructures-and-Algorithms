6f18df2a89b7f339115719b4b3d70703b8d4bd71
Sivarajan <citysiva180@users.noreply.github.com>
2022-11-02 06:51:15 +0530

    Completed Doubly Linked List :-)

--- DS-Deep-Leanring/doubly_linked_list.py
+++ DS-Deep-Leanring/doubly_linked_list.py
@@ -70,10 +70,12 @@ class DoublyLinkedList:
         self.length += 1
         return True
 
-    def pop_first(self, value):
+    def pop_first(self):
+        # if you have no items in the list
         if self.length == 0:
             return None
         temp = self.head
+        # if you have only 1 item in the list
         if self.length == 1:
             self.head = None
             self.head = None
@@ -84,24 +86,128 @@ class DoublyLinkedList:
         self.length -= 1
         return temp
 
+    def get(self, index):
+        # if the index is less than 0 or greater / equal to self.length
+        # Then you need to return nothing
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        if index < self.length/2:
+            for _ in range(index):
+                temp = temp.next
+            return temp
+        else:
+            temp = self.tail
+            # reverse loop
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+            return temp
 
-ddl_1 = DoublyLinkedList(7)
-ddl_1.print_list()
+    def set(self, index, value):
+        # reuse the code from get... gentle and smart
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def insert_value(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.length
+        if index == self.length:
+            return self.append(value)
+
+        new_node = Node(value)
+        before = self.get(index-1)
+        after = before.next
+
+        new_node.prev = before
+        new_node.next = after
+        before.next = new_node
+        after.prev = new_node
+
+    def remove_item(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+
+        temp = self.get(index)
+        temp.next.prev = temp.prev
+        temp.prev.next = temp.next
+        temp.next = None
+        temp.prev = None
+
+        self.length -= 1
+        return temp
+
+
+ddl_1 = DoublyLinkedList(0)
+
+# Adding elements in the ddl
+
+print("Adding Elements in a doubly linked list")
+
+ddl_1.append_item(1)
+ddl_1.append_item(2)
+ddl_1.append_item(3)
+ddl_1.append_item(4)
 ddl_1.append_item(5)
+ddl_1.append_item(6)
+ddl_1.append_item(7)
+ddl_1.append_item(8)
+ddl_1.append_item(9)
+ddl_1.append_item(10)
+
+# Printing the entire list
+
 ddl_1.print_list()
+
+# Poping an item in the list
+
+print("Popping an Item - Removes element from last ")
+
 ddl_1.pop_item()
 ddl_1.print_list()
 
-# if self.length == 0:
-#     return None
-# temp  = self.tail
-# self.tail = self.tail.prev
-# self.tail.next  = None
-# temp.prev = None
-# self.length -= 1
-# if self.length == 0:
-#     self.head = None
-#     self.head = None
-print("prepend")
-ddl_1.prepend(3)
+# Prepending an Item
+print("Prepending an Item - Adds element at first")
+
+ddl_1.prepend(100)
+ddl_1.print_list()
+
+# Pop first
+print("Pop-First - Removing an element from the front")
+
+ddl_1.pop_first()
+ddl_1.print_list()
+
+# Set
+
+print("Set - Setting an element at a certain index")
+
+ddl_1.set(3, 100)
+ddl_1.print_list()
+
+# Get
+
+print("Get - Reading an element at certain index")
+
+new_num = ddl_1.get(3)
+print(new_num.value)
+
+# Insert
+
+print("Insert an element in the middle")
+ddl_1.insert_value(5, 2000)
+ddl_1.print_list()
+
+# Remove
+
+print("Removing an item")
+ddl_1.remove_item(4)
 ddl_1.print_list()
