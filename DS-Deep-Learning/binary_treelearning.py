d3c6a69a1267369dc5e927e610483ce9031403d2
Sivarajan <citysiva180@users.noreply.github.com>
2023-02-01 09:18:02 +0530

    Depth first and Breadth first searches complete

--- DS-Deep-Learning/binary_treelearning.py
+++ DS-Deep-Learning/binary_treelearning.py
@@ -212,6 +212,74 @@ class BinarySearchTree:
     def delete_node(self, value):
         self.__delete_node(self.root, value)
 
+    # ===================================#
+    #  BINARY SEARCH BREADTH FIRST SEARCH
+    # ===================================#
+
+    def BFS(self):
+        current_node = self.root
+        queue = []
+        results = []
+        queue.append(current_node)
+
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+
+        return results
+
+    # ===================================#
+    #  BINARY SEARCH DEPTH FIRST SEARCH
+    # ===================================#
+
+    def dfs_pre_order(self):
+        results = []
+
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results
+
+    # ===================================#
+    #  BINARY SEARCH DEPTH FIRST SEARCH
+    # ===================================#
+
+    def dfs_post_order(self):
+        results = []
+
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+        traverse(self.root)
+        return results
+
+    # ==========================================#
+    #  BINARY SEARCH DEPTH FIRST SEARCH IN ORDER
+    # ==========================================#
+
+    def dfs_in_order(self):
+        results = []
+
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            results.append(current_node.value)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results
+
 
 my_tree = BinarySearchTree()
 
